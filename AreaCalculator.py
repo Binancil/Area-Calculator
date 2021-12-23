6677364a53887984fc033213b381681acc9e6dd0
Binancil <96588111+Binancil@users.noreply.github.com>
2021-12-23 14:09:31 -0800

    Add files via upload

---
+++ AreaCalculator.py
@@ -0,0 +1,9 @@
+import time
+
+width = float(input("Enter the width: "))
+height = float(input("Enter the hight: "))
+area = width * height
+print("Calculating..")
+time.sleep(2)               
+print("\n\nAlright. After careful consideration I have deduced that the area is",area , ".")
+end = input("\nPress enter to exit program.")
